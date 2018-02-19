4f1dba0d6578a10d2b0ff99f4a179aedb8080cb3
TeeSeal <teesealz@gmail.com>
2018-02-19 21:56:46 +0200

    Decide amount of loops by duration

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ async function fetchCoub(text, format = 'mp4') {
 
   return coub
     .attachAudio()
-    .loop(2)
+    .loop(Math.floor(20 / coub.duration))
     .addOption('-c', 'copy')
     .addOption('-shortest')
     .write(filePath)
