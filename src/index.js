cd3e601b3031240e9c532185f5db6777672460f1
TeeSeal <teesealz@gmail.com>
2018-02-19 22:29:18 +0200

    chore: add help command

--- src/index.js
+++ src/index.js
@@ -9,12 +9,12 @@ const coubRegexp = /(https?:\/\/)?(www\.)?coub\.com\/[\w/]+/
 async function fetchCoub(text, format = 'mp4') {
   const url = text.match(coubRegexp)[0]
   const coub = await Coub.fetch(url)
-  const filePath = path.join(__dirname, '../.coub.mp4')
+  const filePath = path.join(__dirname, `../.coub.${format}`)
 
   if (format === 'gif') {
     return coub
       .scale(500)
-      .write('.coub.gif')
+      .write(filePath)
   }
 
   return coub
@@ -48,6 +48,15 @@ client
     if (command === 'ping') {
       const response = await msg.channel.send('Pong!')
       return response.edit(`Pong! ${response.createdAt - msg.createdAt} ms`)
+    }
+
+    if (command === 'help') {
+      return msg.reply([
+        'my only command is `ping`, so my owner can check if I\'m not dead, haha.',
+        'Just send a coub link anywhere I can see it and I will do my best to embed it for you.',
+        'You can wrap the link in `<` and `>` to prevent Discord from showing its inferior embed and only let mine through',
+        'As a bonus, you can add the keyword `gif` to your message and I will make it into a gif insead :wink:.'
+      ].join('\n'))
     }
   })
 
